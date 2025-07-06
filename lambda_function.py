c02ae8c901c03151126308c1b7c3a28df122eabc
Brunodias37 <62164559+Brunodias37@users.noreply.github.com>
2025-07-06 17:39:29 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,14 +3,22 @@ import requests
 import pandas as pd
 from datetime import datetime
 import s3fs
+import boto3
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 data_pregao = datetime.today().strftime("%Y%m%d")
 
 def salvar_dataframe_parquet_s3(df, bucket, s3_key, compression='snappy',  **args):
     s3 = s3fs.S3FileSystem()
-    full_s3_path = f's3://{bucket}/{s3_key}'
-
-    df.to_parquet(full_s3_path, engine='pyarrow', compression=compression, storage_options={'s3_additional_kwargs': {'ACL': 'bucket-owner-full-control'}}, **args)
-
+    df.to_parquet(
+        f's3://{bucket}/{s3_key}/',
+        engine='pyarrow',
+        filesystem=s3,
+        partition_cols=['anomesdia']
+    )
 
 def process_registro(registro):
     # Corrigir campo 'segment'
@@ -29,6 +37,61 @@ def process_registro(registro):
     registro['data_pregao'] = data_pregao
     return registro
 
+
+def criar_tabela_raw(bucket, s3_key):
+    database_name = "workspace_db"
+    table_name = "tb_dados_b3_raw"
+    table_location = f's3://{bucket}/{s3_key}/'
+    glue_client = boto3.client("glue")
+
+    try:
+        glue_client.get_database(Name=database_name)
+    except glue_client.exceptions.EntityNotFoundException:
+        glue_client.create_database(DatabaseInput={'Name': database_name})
+
+    table_input = {
+        'Name': table_name,
+        'StorageDescriptor': {
+            'Columns': [
+                {"Name": "segment", "Type": "string"},
+                {"Name": "cod", "Type": "string"},
+                {"Name": "asset", "Type": "string"},
+                {"Name": "type", "Type": "string"},
+                {"Name": "part", "Type": "string"},
+                {"Name": "partAcum", "Type": "string"},
+                {"Name": "theoricalQty", "Type": "string"},
+                # {"Name": "segment_list", "Type": "string"},
+                # {"Name": "type_list", "Type": "string"}
+            ],
+            'Location': table_location,  # sem anomesdia
+            'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
+            'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
+            'SerdeInfo': {
+                'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe',
+                'Parameters': {'serialization.format': '1'}
+            }
+        },
+        'PartitionKeys': [{"Name": "anomesdia", "Type": "string"}],
+        'TableType': 'EXTERNAL_TABLE',
+        'Parameters': {'classification': 'parquet'}
+    }
+
+    try:
+        glue_client.get_table(DatabaseName=database_name, Name=table_name)
+        glue_client.update_table(DatabaseName=database_name, TableInput=table_input)
+    except glue_client.exceptions.EntityNotFoundException:
+        glue_client.create_table(DatabaseName=database_name, TableInput=table_input)
+
+
+def reparar_tabela_athena(database_name, table_name):
+    athena = boto3.client('athena')
+    output_location = 's3://bovespa-204590505567/athena-query-results/'  # crie este prefix
+    response = athena.start_query_execution(
+        QueryString=f"MSCK REPAIR TABLE {database_name}.{table_name}",
+        QueryExecutionContext={'Database': database_name},
+        ResultConfiguration={'OutputLocation': output_location}
+    )
+    logger.info(f"MSCK REPAIR TABLE iniciado. QueryExecutionId: {response['QueryExecutionId']}")
 
 
 def lambda_handler(event, context):
@@ -57,9 +120,11 @@ def lambda_handler(event, context):
 
     # Criar DataFrame
     df = pd.DataFrame(registros_processados)
-
-    salvar_dataframe_parquet_s3(df, 'bovespa-204590505567/raw', f'dados_ibov_{data_pregao}.parquet')
-
+    df = df.rename(columns={'data_pregao': 'anomesdia'})
+    df = df.drop(columns=['type_list', 'segment_list'])
+    salvar_dataframe_parquet_s3(df, 'bovespa-204590505567', 'raw/tb_dados_b3_raw')
+    criar_tabela_raw('bovespa-204590505567', 'raw/tb_dados_b3_raw')
+    reparar_tabela_athena('workspace_db', 'tb_dados_b3_raw')
     return {
         'statusCode': 200,
         'body': json.dumps(f'Arquivo com dados de {datetime.today().strftime("%Y/%m/%d")} salvo com sucesso! ')
